e3445a31f6ab4e7068172ce3f088272c0699f547
James Lambie <jameslambie@gmail.com>
2017-02-15 16:26:41 +0800

    feat: implement insert method

--- test/unit/couchdb.js
+++ test/unit/couchdb.js
@@ -33,7 +33,79 @@ describe('CouchDB', function () {
     })
 
     it('should load config from options supplied')
-
-    it('should have readyState == 0 when initialised')
   })
+
+  describe('connect', function () {
+    it('should create specified collection if it doesn\'t exist', function (done) {
+      var couchdb = new CouchDBAdapter()
+
+      couchdb.connect({ collection: 'users' }).then(() => {
+        // var collection = couchdb.database.getCollection('users')
+        // should.exist(collection)
+        // collection.name.should.eql('users')
+        done()
+      })
+    })
+
+    it('should have readyState == 1 when connected', function (done) {
+      var couchdb = new CouchDBAdapter()
+      couchdb.connect({ collection: 'posts' }).then(() => {
+        couchdb.readyState.should.eql(1)
+        done()
+      }).catch(err => {
+        done(err)
+      })
+    })
+  })
+
+  describe('insert', function () {
+  it('should insert a single document into the database', function (done) {
+    var couchdb = new CouchDBAdapter()
+    couchdb.connect({ collection: 'users' }).then(() => {
+      var user = { name: 'Ernest' }
+
+      couchdb.insert(user, 'users', {}).then((results) => {
+        results.constructor.name.should.eql('Array')
+        results[0].name.should.eql('Ernest')
+        done()
+      }).catch(err => {
+        done(err)
+      })
+    })
+  })
+
+  it('should insert an array of documents into the database', function (done) {
+    var couchdb = new CouchDBAdapter()
+    couchdb.connect({ collection: 'users' }).then(() => {
+      var users = [{ name: 'Ernest' }, { name: 'Wallace' }]
+
+      couchdb.insert(users, 'users', {}).then((results) => {
+        results.constructor.name.should.eql('Array')
+        results.length.should.eql(2)
+        results[0].name.should.eql('Ernest')
+        results[1].name.should.eql('Wallace')
+        done()
+      }).catch(err => {
+        done(err)
+      })
+    })
+  })
+})
+
+describe('find', function () {
+  it('should find a single document in the database', function (done) {
+    var couchdb = new CouchDBAdapter()
+    couchdb.connect({ collection: 'users' }).then(() => {
+      var users = [{ name: 'Ernest' }, { name: 'Wallace' }]
+
+      couchdb.insert(users, 'users', {}).then((results) => {
+        couchdb.find({ name: 'Wallace' }, 'users', {}).then((results) => {
+          results.constructor.name.should.eql('Array')
+          results[0].name.should.eql('Wallace')
+          done()
+        })
+      })
+    })
+  })
+})
 })
